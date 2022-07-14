b2d0f08395e86b2d44e045306539b494230fd50a
chiwoogood <chiwooooo@naver.com>
2022-07-14 15:43:03 +0900

    8th file upload

---
+++ Codeup_basics/File_08/codeup_51.py
@@ -0,0 +1,7 @@
+a, b = input().split(' ')
+a = int(a)
+b = int(b)
+if a == b:
+    print(False)
+else:
+    print(True)
---
+++ Codeup_basics/File_08/codeup_55.py
@@ -0,0 +1,7 @@
+a,b = input().split(' ')
+a = int(a)
+b = int(b)
+if bool(a) | bool(b):
+    print(True)
+else:
+    print(False)
